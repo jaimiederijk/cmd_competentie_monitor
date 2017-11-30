1d9cd3aec088fe6ed213a1553e740b06021b16f4
jaimiederijk <j.r.derijk@quicknet.nl>
2017-11-30 01:24:49 +0100

    more for testing

--- prototype1/datatest/public/javascript/newformhandler.js
+++ prototype1/datatest/public/javascript/newformhandler.js
@@ -1,4 +1,5 @@
 var formexample = require('./formexample.js');
+var termsFormHandler = require('./termsformhandler.js');
 
 var htmlElements = {
   body: document.querySelector('body'),
@@ -10,13 +11,23 @@ var htmlElements = {
 
 var state = {
   type:"form",
-  name: "new form",
+  formName: "new form",
 
+  sections: [
+
+  ]
 
 }
 
+
+
 var formState = {
   index:0,
+  prevIndex:0
+}
+
+var sectionState = {
+
 }
 
 var listeners = {
@@ -28,7 +39,9 @@ var listeners = {
         if(this !== prev) {
             prev = this;
         }
-        console.log(this.value);
+        if (this.value == "false") {
+            formState.index++;
+        }
       })
     }
   },
@@ -42,9 +55,21 @@ var listeners = {
   },
   submitField : function () {
     for (var i = 0; i < htmlElements.fieldButtons.length; i++) {
+
       htmlElements.fieldButtons[i].disabled = true;
       htmlElements.fieldButtons[i].addEventListener('click', function() {
+        var key="";
+        var value="";
+        var input="input";
         formState.index++;
+
+        if(!event.target.parentElement.querySelector("input")) {
+          input="textarea";
+        }
+        value = event.target.parentElement.querySelector(input).value
+        key = event.target.parentElement.querySelector(input).name
+
+        helperFuntions.processForm(key, value);
         helperFuntions.displayActiveField();
       })
     }
@@ -54,17 +79,51 @@ var listeners = {
 
 var helperFuntions = {
   displayActiveField : function () {
-    for (var i = 0; i < htmlElements.fields.length; i++) {
-      if (i==formState.index) {
-        htmlElements.fields[i].classList.remove("hidden");
-        if (i!=0 || i != htmlElements.fields.length-1) {
-          htmlElements.fields[i-1].classList.add("hidden");
-        }
+    debugger
+    htmlElements.fields[formState.index].classList.remove("hidden");
+    htmlElements.fields[formState.prevIndex].classList.add("hidden");
+    formState.prevIndex=formState.index;
+    // for (var i = 0; i < htmlElements.fields.length; i++) {
+    //   if (i==formState.index) {
+    //     htmlElements.fields[i].classList.remove("hidden");
+    //     if (i!=0 || i != htmlElements.fields.length-1) {
+    //       htmlElements.fields[i-1].classList.add("hidden");
+    //     }
+    //
+    //   }
+    //
+    // }
 
+  },
+  processForm : function (key, answer) {
+    var combinedState = state;
+    //
+
+    //add subsubjects
+    if (key == "subsubjects") {
+      var arr = answer.split(/[\n\r]/g);
+      sectionState.subSubjects = [];
+
+      for (var i = 0; i < arr.length; i++) {
+        sectionState.subSubjects.push(
+          {
+            subSubjectsName: arr[i]
+          }
+        );
       }
+    }
+    if (key == "terms") {
 
     }
+    //debugger
+    //add subject
+    if (key == "subject") {
+      sectionState.subject = answer;
+      combinedState.sections.push(sectionState);
+    }
 
+
+    formexample.createForm(combinedState);
   }
 }
 
@@ -72,8 +131,8 @@ var newformhandler = function () {
   listeners.radioBtnChange();
   listeners.submitField();
   listeners.fieldsetInputChange();
-  debugger
-  formexample();
+
+
 }
 
 module.exports = newformhandler;
